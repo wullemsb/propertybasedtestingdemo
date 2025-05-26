c3dd283e90e9f9e267bf8df059d07e7473b55ab7
Wullems Bart <wullemsb@gmail.com>
2025-05-26 15:29:01 +0200

    Value tuple support

--- PropertyBasedTesting.Tests/BuiltInGeneratorsTests.cs
+++ PropertyBasedTesting.Tests/BuiltInGeneratorsTests.cs
@@ -12,18 +12,16 @@ namespace PropertyBasedTesting.Tests
 {
     public class BuiltInGeneratorsTests
     {
-        //No out-of-the-box support for value tuple(yet)
         [Property(Verbose =true)]
         public Property Adding_Two_PositiveNumbers_Doesnt_Depend_On_Parameter_Order_1(PositiveInt x, PositiveInt y)
         {
             return (Add(x.Get, y.Get) == Add(y.Get, x.Get)).ToProperty();
         }
 
-        //No out-of-the-box support for value tuple(yet)
         [Property]
-        public Property Adding_Two_Numbers_Doesnt_Depend_On_Parameter_Order_1(Tuple<int, int> values)
+        public Property Adding_Two_Numbers_Doesnt_Depend_On_Parameter_Order_1((int a, int b) values)
         {
-            return (Add(values.Item1, values.Item2) == Add(values.Item1, values.Item2)).ToProperty();
+            return (Add(values.a, values.b) == Add(values.a, values.b)).ToProperty();
         }
 
         [Property(Verbose = true)]
@@ -42,14 +40,6 @@ namespace PropertyBasedTesting.Tests
         public Property Generator2(NonEmptyArray<string> values)
         {
             return values.Get.Any(v => !string.IsNullOrEmpty(v)).ToProperty();
-        }
-
-        //No out-of-the-box support for value tuple(yet)
-        // Will fail
-        [Property]
-        public Property Adding_Two_Numbers_Doesnt_Depend_On_Parameter_Order_2((int x, int y) values)
-        {
-            return (Add(values.x, values.y) == Add(values.y, values.x)).ToProperty();
         }
     }
 }
